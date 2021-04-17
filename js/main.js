1cd9817fbfaf2030995573725ddbb2537a118f83
Bennett B Madavana <bennettmadavana@gmail.com>
2021-04-17 12:02:32 +0530

    Add movie details feature

--- js/main.js
+++ js/main.js
@@ -128,7 +128,7 @@ function getMovies(str){
             <div class="well text-center">
             <img src="https://image.tmdb.org/t/p/original${movie.poster_path}">
             <h5 class="movieTitle" data-toggle="tooltip" data-placement="bottom" title="${movie.title}">${movie.title}</h5>
-            <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary details" href="#">Movie Details</a>
+            <a onclick="movieSelected('${movie.id}')" class="btn btn-primary details" href="#">Movie Details</a>
             </div>
             </div>
             `;
@@ -140,32 +140,48 @@ function getMovies(str){
     });
 }
 
-// document.getElementById("random").addEventListener("click", function (){
-//     const characters = 'abcdefghijklmnopqrstuvwxyz';
-//     let c = characters[Math.floor(Math.random()*25)];
-//     let year = Math.floor(Math.random() * 30 + 1990);
-//     console.log(c);
-//     axios.get('http://www.omdbapi.com/?apikey=3e7e5973&t='+ c +'&y=' + year)
-//     .then((response) => {
-//         console.log(response);
-//         console.log(response.data.Title);
-//         let movie = response.data;
-//         let output = '';
-//         output += `
-//             <div class="col-md-3">
-//             <div class="well text-center">
-//             <img src="${movie.Poster}">
-//             <h5>${movie.Title}</h5>
-//             <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
-//             </div>
-//             </div>
-//             `;
-//             $('#movies').html(output);
-//     })
+function movieSelected(id){
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    getMovie();
+    return false;
+}
+
+function getMovie(){
+    let movieId = sessionStorage.getItem('movieId');
+    axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${api_key}&language=en-US`)
+    .then((response) =>{
+        console.log(response);
     
-//     .catch((err) => {
-//         console.log(err);
-//     });
+        let movie = response.data;
+        let output = `
+        <div class="row">
+          <div class="col-md-4">
+            <img src="https://image.tmdb.org/t/p/original${movie.poster_path}" class="thumbnail">
+          </div>
+          <div class="col-md-8">
+            <h2>${movie.title}</h2>
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Released:</strong> ${movie.release_date}</li>
+              <li class="list-group-item"><strong>Rated:</strong> ${movie.vote_average}</li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well">
+            <h3>Plot</h3>
+            ${movie.overview}
+            <hr>
+            <a href="http://imdb.com/title/${movie.imdb_id}" target="_blank" class="btn btn-primary">View IMDB</a>
+            <a href="genre.html" class="btn btn-default">Go Back?</a>
+          </div>
+        </div>
+      `;
+
+        $('#movie').html(output);
+    })
+        .catch((err) => {
+            console.log(err);
+        });
     
-    
-// });
+}
